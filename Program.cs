2f1a37a19549744763841c1ddf406aaf4ecfcb9a
Zikmian <z1muem@yandex.ru>
2023-04-16 20:45:06 +0900

    try to fix git 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a, b, c;
+string input_a, input_b, input_с;
+input_a = Console.ReadLine();
+a = int.Parse(input_a);
+input_b = Console.ReadLine();
+b = int.Parse(input_b);
+
+// задача 2
+if (a > b) 
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
+// задача 4
+a = int.Parse(input_a);
+input_b = Console.ReadLine();
+b = int.Parse(input_b);
+input_с = Console.ReadLine();
+c = int.Parse(input_с);
+
+
